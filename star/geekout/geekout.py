9d0d7ecc2c5b503e26ba63bc51877fbb80ae192c
outboxltd <benatiya@gmail.com>
2019-07-08 10:15:07 +0300

    geekout

---
+++ star/geekout/geekout.py
@@ -0,0 +1,20 @@
+sentence = "Hello, my name is Inigo Montoya"
+
+hello = slice(0, 5)
+my_name = slice(7,14)
+# Hlo_ynm_sIioMnoa = slice(0,1)
+H = slice(0,1)
+lo = slice(3,5)
+Y = slice(8,9)
+# print(sentence[hello])
+# print(sentence[my_name])
+p1 = sentence[H]
+p2 = sentence[lo]
+p3 = "_"
+p4 = sentence[Y]
+
+print(p1 + p2 + p3 + p4)
+
+# you got the idea :)
+
+
