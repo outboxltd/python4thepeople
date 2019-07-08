d0902a9cd2f75bb7226b5deb66158761af91c193
outboxltd <benatiya@gmail.com>
2019-07-08 05:21:07 +0300

    ex2 star

---
+++ 4fun/1.py
@@ -0,0 +1,12 @@
+# def say_hello(name):
+#  print("Hello " + name)
+# say_hello("gilad")
+
+
+def donuts(count):
+    if count < 10:
+        print ("Number of donuts: {0}.".format(count))
+    else:
+        print("Number of donuts:many")
+
+donuts(30)
---
+++ 4fun/2.py
@@ -0,0 +1,6 @@
+    
+def both_ends(str):
+    print(str[0] + str[1] + str[-1] + str[-2])
+
+
+both_ends("gilad")
