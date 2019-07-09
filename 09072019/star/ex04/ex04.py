0eb50ec212533e0890cc37e5b39e6122e1370002
outboxltd <benatiya@gmail.com>
2019-07-09 17:05:16 +0300

    Update ex04.py

--- 09072019/star/ex04/ex04.py
+++ 09072019/star/ex04/ex04.py
@@ -1,5 +1,5 @@
 quote = "some people drink from the fountain of knowledge, others just gargle."
 VOWELS = ["a", "e", "i", "o", "u"]
 
-whare_are_the_vowels = [i for (i, value) in enumerate(quote) if value in VOWELS]
+whare_are_the_vowels = [i for (i, the_letter) in enumerate(quote) if the_letter in VOWELS]
 print(whare_are_the_vowels)
